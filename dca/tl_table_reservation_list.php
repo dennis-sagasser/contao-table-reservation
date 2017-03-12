bfb74c502c867f09d0c1478e1c7447f13a1c1623
Dennis Sagasser <dennis.sagasser@gmail.com>
2017-03-12 12:26:54 +0100

    Fixes #10 - FR: Make reservation editable / Insert new reservations

--- dca/tl_table_reservation_list.php
+++ dca/tl_table_reservation_list.php
@@ -130,7 +130,7 @@ $GLOBALS['TL_DCA']['tl_table_reservation_list'] = array
         (
             'inputType' => 'listWizard',
             'label'     => &$GLOBALS['TL_LANG']['tl_table_reservation_list']['seats'],
-            'sql'       => "text NOT NULL",
+            'sql'       => "blob NOT NULL",
             'eval'      => array
             (
                 'mandatory' => true,
