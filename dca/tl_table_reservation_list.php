271851a903adcdc81a5d08813d219bea6cba0a52
Dennis Sagasser <dennis.sagasser@gmail.com>
2017-03-28 22:31:25 +0200

    Delete reservation confirmation

--- dca/tl_table_reservation_list.php
+++ dca/tl_table_reservation_list.php
@@ -80,7 +80,7 @@ $GLOBALS['TL_DCA']['tl_table_reservation_list'] = [
                 'label'      => &$GLOBALS['TL_LANG']['tl_table_reservation_list']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['tl_table_reservation_list']['deleteConfirm'] . '\'])return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['tl_table_reservation_list']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"',
             ],
         ]
     ],
